37e2eb9f61a27ab3331a892ac5531f97f11efa29
Nick Young <usaussie@gmail.com>
2012-03-07 10:49:54 -0500

    ready for first release

--- primermanager.js
+++ primermanager.js
@@ -2,49 +2,48 @@ Drupal.behaviors.primermanager = function(context) {
 		
 	if($('#edit-primermanager-selected-operation').length > 0) {
 	
-		$("#edit-primermanager-name").parent().hide();
-		$("#edit-primermanager-local-configurations").parent().hide();
+		$("#edit-primermanager-name-wrapper").hide();
+		$("#edit-primermanager-local-configurations-wrapper").hide();
+		$("#edit-primermanager-import-settings-wrapper").hide();
 		$("#edit-submit").hide();
 		
 		$('#edit-primermanager-selected-operation').change(function() {
 			
-			if($('#edit-primermanager-selected-operation').val() == 'select') {
-				
-				$("#edit-primermanager-name").parent().hide();
-				$("#edit-primermanager-local-configurations").parent().hide();
-				$("#edit-submit").hide();
-				
-			}
+			// hide all fields then based on the choice, show them using the statements below
+			$("#edit-primermanager-name-wrapper").hide();
+			$("#edit-primermanager-local-configurations-wrapper").hide();
+			$("#edit-primermanager-import-settings-wrapper").hide();
+			$("#edit-submit").hide();
 			
-			if($('#edit-primermanager-selected-operation').val() == 'save') {
-				
-				$("#edit-primermanager-name").parent().show();
-				$("#edit-primermanager-local-configurations").parent().hide();
+			var selection = $(this).val();
+			
+			switch (selection) {
+			
+			case 'save':
+				$("#edit-primermanager-name-wrapper").show();
+				$("#edit-submit").show();
+				break;
+			case 'share':
+				$("#edit-primermanager-name-wrapper").show();
+				$("#edit-submit").show();
+				break;
+			case 'import':
+				$("#edit-primermanager-import-settings-wrapper").show();
+				$("#edit-submit").show();
+				break;
+			case 'export':
+				$("#edit-primermanager-name-wrapper").show();
+				$("#edit-submit").show();
+				break;
+			case 'delete':
+				$("#edit-primermanager-local-configurations-wrapper").show();
 				$("#edit-submit").show();
 				
 			}
-			
-			if($('#edit-primermanager-selected-operation').val() == 'export') {
-				
-				$("#edit-primermanager-local-configurations").parent().hide();
-				$("#edit-primermanager-name").parent().show();
-				$("#edit-submit").show();
-				
-			}
-			
-			if($('#edit-primermanager-selected-operation').val() == 'delete') {
-				
-				$("#edit-primermanager-name").parent().hide();
-				$("#edit-primermanager-local-configurations").parent().show();
-				$("#edit-submit").show();
-				
-			}
-			
+
 			
 		});
 		
 	}
-
-		
 	
 }
